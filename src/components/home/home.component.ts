f7a7b6faaf088e053a8df9f21ef7a71f5a99fc96
João Faria <a25590@alunos.ipca.pt>
2024-01-19 10:09:35 +0000

    home_frontend

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,54 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(private http: HttpClient, private toastr: ToastrService) {}
+  restaurants: any = {};
+  currentPage: number = 1;
+  itemsPerPage: number = 4;
+  
+  constructor(private http: HttpClient, private toastr: ToastrService, private router: Router) {}
+
+  get totalPages() {
+    return Math.ceil(this.restaurants.length / this.itemsPerPage);
+  }
+
+  get paginatedRestaurants() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.restaurants.slice(startIndex, endIndex);
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    const totalPages = Math.ceil(this.restaurants.length / this.itemsPerPage);
+    if (this.currentPage < totalPages) {
+      this.currentPage++;
+    }
+  }
+  
+  ngOnInit() {
+    this.getRestaurants();
+  }
+
+  getRestaurants() {
+    this.http.get('http://localhost:3005/restaurants').subscribe(
+      (res) => {
+        this.restaurants = res;
+        console.log(this.restaurants);
+      },
+      (error) => {
+        console.error('Erro ao obter restaurantes: ', error);
+      }
+    );
+  }
+
+  goToRestaurantPreview(restaurantId: number) {
+    this.router.navigate(['/Restaurants/restaurants-preview', restaurantId]);
+  }
 
   showSuccess() {
     this.toastr.success('Contacto efetuado com sucesso!', 'Sucesso', {
