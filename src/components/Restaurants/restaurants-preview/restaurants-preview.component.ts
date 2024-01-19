d1bbe71cb3472f10abd8250ef596a30acca08496
goncalo03 <gcoutinho401@gmail.com>
2024-01-19 09:59:44 +0000

    get restaurant por id

--- src/components/Restaurants/restaurants-preview/restaurants-preview.component.ts
+++ src/components/Restaurants/restaurants-preview/restaurants-preview.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-restaurants-preview',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsPreviewComponent implements OnInit {
 
-  constructor() { }
+  restaurantId: any = {};
+  restaurantData: any;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.restaurantId = +params['id'];
+      this.getRestaurantInfo();
+    });
   }
 
+  getRestaurantInfo() {
+    this.http.get('http://localhost:3005/restaurants').subscribe(
+      (res: any) => {
+        console.log('Response from server:', res);
+
+        const selectedRestaurant = res.find((restaurant:any) => restaurant.id === this.restaurantId);
+
+        if (selectedRestaurant) {
+          this.restaurantData = selectedRestaurant;
+          console.log('Restaurant data:', this.restaurantData);
+        } else {
+          console.error('Restaurante não encontrado. Restaurant ID:', this.restaurantId);
+        }
+      },
+      (error) => {
+        console.error('Erro ao obter restaurantes: ', error);
+      }
+    );
+  }
 }
