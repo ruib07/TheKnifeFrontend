fc0c573a7da9d60adbb85136a8ad8164447eb6fb
goncalo03 <gcoutinho401@gmail.com>
2024-01-24 13:01:31 +0000

    comentários quase terminados

--- src/components/Restaurants/restaurants-preview/restaurants-preview.component.ts
+++ src/components/Restaurants/restaurants-preview/restaurants-preview.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, LOCALE_ID, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
@@ -15,11 +16,18 @@ export class RestaurantsPreviewComponent implements OnInit {
   restaurantId: any = {};
   restaurantData: any;
   minDate: any;
+  user: any = {};
+  comments: any = {};
+  restaurantComments: any[] = [];
+
+  private datePipe = new DatePipe('en-US');
 
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private el: ElementRef,
+    private toastr: ToastrService
   ) {
     registerLocaleData(localePt, 'pt-BR'),
       (this.minDate = {
@@ -33,6 +41,7 @@ export class RestaurantsPreviewComponent implements OnInit {
     this.route.params.subscribe((params) => {
       this.restaurantId = +params['id'];
       this.getRestaurantInfo();
+      this.getUtilizador();
     });
   }
 
@@ -40,20 +49,50 @@ export class RestaurantsPreviewComponent implements OnInit {
     this.router.navigate(['/reservations']);
   }
 
+  showSuccess() {
+    this.toastr.success('Comentário enviado com sucesso!', 'Sucesso', {
+      progressBar: true,
+      closeButton: true,
+      positionClass: 'toast-bottom-right',
+      timeOut: 5000,
+    });
+  }
+
+  showError() {
+    this.toastr.error('Comentário não enviado!', 'Erro', {
+      progressBar: true,
+      closeButton: true,
+      positionClass: 'toast-bottom-right',
+      timeOut: 5000,
+    });
+  }
+
+  showErrorPont() {
+    this.toastr.error(
+      'Pontuação inválida. A pontuação deve estar entre 0 e 10.',
+      'Erro',
+      {
+        progressBar: true,
+        closeButton: true,
+        positionClass: 'toast-bottom-right',
+        timeOut: 5000,
+      }
+    );
+  }
+
   getRestaurantInfo() {
     this.http.get('http://localhost:3005/restaurants').subscribe(
       (res: any) => {
-        const selectedRestaurant = res.find(
-          (restaurant: any) => restaurant.id === this.restaurantId
-        );
-
+        console.log('Response from server:', res);        
+        
+        const selectedRestaurant = res.find((restaurant:any) => restaurant.id === this.restaurantId);       
+         
         if (selectedRestaurant) {
           this.restaurantData = selectedRestaurant;
+          console.log('Restaurant data:', this.restaurantData);
+          this.getRestaurantComments();
         } else {
-          console.error(
-            'Restaurante não encontrado. Restaurant ID:',
-            this.restaurantId
-          );
+          console.error('Restaurante não encontrado. Restaurant ID:', this.restaurantId);
         }
       },
       (error) => {
@@ -62,7 +101,104 @@ export class RestaurantsPreviewComponent implements OnInit {
     );
   }
 
+  sendComments(comments: { review: number; comment: string }) {
+    if (comments.review < 0 || comments.review > 10) {
+      this.showErrorPont();
+      return;
+    }
+
+    const commentDate = new Date();
+
+    const commentsToSend = {
+      review: comments.review,
+      comment: comments.comment,
+      username: this.user?.username || 'Username Não Disponível',
+      commentdate: commentDate,
+      user_id: this.getUtilizadorId(),
+      restaurant_id: this.restaurantId,
+    };
+
+    this.http.post('http://localhost:3005/comments', commentsToSend).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.showSuccess();
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
+      },
+      (error) => {
+        console.error(error);
+        this.showError();
+      }
+    );
+  }
+
+  getRestaurantComments() {
+    this.http
+      .get(`http://localhost:3005/comments?restaurant_id=${this.restaurantId}`)
+      .subscribe(
+        (res: any) => {
+          this.restaurantComments = res.filter(
+            (comment: any) => comment.restaurant_id === this.restaurantId
+          );
+          console.log('Restaurant comments:', this.restaurantComments);
+        },
+        (error) => {
+          console.error('Erro ao obter comentários do restaurante: ', error);
+        }
+      );
+  }
+
+  getUtilizador() {
+    const usertoken = localStorage.getItem('usertoken');
+
+    if (usertoken) {
+      const headers = new HttpHeaders().set(
+        'Authorization',
+        `Bearer ${usertoken}`
+      );
+
+      this.http
+        .get(`http://localhost:3005/users/${this.getUtilizadorId()}`, {
+          headers,
+        })
+        .subscribe(
+          (res: any) => {
+            this.user = res;
+            console.log(res);
+          },
+          (error) => {
+            console.error('Erro ao obter dados do utilizador: ', error);
+          }
+        );
+    }
+  }
+
+  getUtilizadorId() {
+    const usertoken = localStorage.getItem('usertoken');
+    if (usertoken) {
+      const usertokenParts = usertoken.split('.');
+      if (usertokenParts.length === 3) {
+        const userdecodedToken = atob(usertokenParts[1]);
+        const usertokenInfo = JSON.parse(userdecodedToken);
+        return usertokenInfo.id;
+      }
+    }
+    return null;
+  }
+
   goToReservation(restaurantId: number) {
     this.router.navigate(['reservations', restaurantId]);
+  }
+
+  visibleComments = 3;
+
+  verComentarios() {
+    this.visibleComments += 3;
+  }
+
+  formatDate(date: string): string {
+    const parsedDate = new Date(date);
+    return this.datePipe.transform(parsedDate, 'dd-MM-yyyy') || '';
   }
 }
